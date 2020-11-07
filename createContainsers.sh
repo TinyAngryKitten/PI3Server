eb320924850fddcbe4678e9871a300ce25e00b84
sander høyvik <sander.hoyvik@gmail.com>
2020-11-07 18:55:25 +0100

    add homebridge

--- createContainsers.sh
+++ createContainsers.sh
@@ -1,5 +1,6 @@
 mkdir $(pwd)/etc-pihole
 mkdir $(pwd)/etc-dnsmasq.d
+mkdir $(pwd)/homebridge
 
 export WEBPASSWORD="maybeputapasswordhere"
 
@@ -18,6 +19,19 @@ docker run -d \
     -e PROXY_LOCATION="pi.hole" \
     -e ServerIP="127.0.0.1" \
     pihole/pihole:latest
+
+docker run -d \
+    --name homebridge \
+    --network host \
+    --dns=127.0.0.1 --dns=1.1.1.1 \
+    --restart=unless-stopped \
+    -e TZ="Europe/Oslo" \
+    -e PGID="1000" \
+    -e PUID="1000" \
+    -e HOMEBRIDGE_CONFIG_UI="1" \
+    -e HOMEBRIDGE_CONFIG_UI_PORT="8080" \
+    -v "$(pwd)/homebridge:/homebridge" \
+    oznu/homebridge:latest
 
 sudo apt -y install openjdk-8-jdk-headless
 
